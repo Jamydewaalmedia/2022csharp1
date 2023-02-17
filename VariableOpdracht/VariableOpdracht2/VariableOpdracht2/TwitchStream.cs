c03ea94eb4a8e73944b9b4250f1884204a706f20
jamydewaal <35739@ma-web.nl>
2023-02-17 17:31:46 +0100

    gemaakt tot en met rekenopdr en variable niet helemaal af

---
+++ VariableOpdracht/VariableOpdracht2/VariableOpdracht2/TwitchStream.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace VariableOpdracht2
+{
+    internal class TwitchStream
+    {
+        //maak hier class variables aan:
+        class variables
+        {
+            string name = "Sykunno";
+            string lastStreamName = "dog";
+            Game streamGame;
+            int followers = 0;
+            bool automod = false;
+            
+
+
+        }
+        // type=string, naam: name,
+        // type=string, naam: lastStreamName
+        // type=Game, naam: streamGame
+        // type=int, naam: followers;
+        // type=bool, naam: automod
+        
+    }
+}
